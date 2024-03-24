94b68218b68ecfd7340a51d4f81177ea17123647
karthikn25 <mkarthikeyan179@gmail.com>
2024-03-24 15:49:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { data } from "./Data/data";
 
 function App() {
+//  const data = ["doing yoga","drinking tea","bathing","eating breakfast","start learning","doing type writing practice"];
+ const [arr,setArr]=useState(data);
+ const [msg,setMsg]=useState();
+
+ const handleNewData = (e)=>{
+//  e.preventDefault();
+//  data.push(newdata);
+const add = {
+ msg:msg
+}
+//  setArr(data,newdata);
+  setArr([...arr,add])
+ }
+
+ const handleRemove = (dataId)=>{
+ const remaingData = arr.filter((d,idx)=>idx !==dataId)
+ setArr(remaingData)
+ }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1 id="head">Todo's</h1>
+        <div className="input-box">
+          <input type="text" value={msg} onChange={(e)=>setMsg(e.target.value)}/>
+          <i class='bx bx-add-to-queue' onClick={handleNewData}></i>
+        </div>
+        {
+          arr.length!==0 ? <div className="data-box">
+        
+          {
+             arr.map((d,idx)=>(
+            <div className="content-box" key={idx}>
+            
+              <p>{d.msg}</p>
+              
+              <i class='bx bx-trash-alt' onClick={()=>handleRemove(idx)}></i>
+
+            </div>
+          
+            ))
+            
+          }
+        
+          
+          </div>
+          :<></>
+        }
+        
+        </div>
     </div>
   );
 }
---
+++ src/Data/data.js
@@ -0,0 +1,15 @@
+export const data = [{
+    msg:"doing yoga"
+},
+{
+    msg:"reading news paper"
+},
+{
+    msg:"finish breakfast"
+},
+{
+    msg:"type writing practice"
+},
+{
+    msg:"new learnings"
+}]
